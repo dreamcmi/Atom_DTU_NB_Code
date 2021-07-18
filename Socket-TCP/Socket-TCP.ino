484741241ce5be172092cd21a5ce1c9fa4a1e360
dreamcmi <1912544842@qq.com>
2021-07-18 21:07:43 +0800

    fix: socket创建参数应为三个

--- Socket-TCP/Socket-TCP.ino
+++ Socket-TCP/Socket-TCP.ino
@@ -8,7 +8,7 @@
 
 ATOM_DTU_NB DTU;
 String readstr;
-String socketId = "1";
+String socketId = "0";
 String port = "37168";
 String url = "112.125.89.8";
 
@@ -47,19 +47,19 @@ void loop()
   readstr = DTU.waitMsg(1000);
   Serial.print(readstr);
 
-  //创建tcp，id=1
-  DTU.sendMsg("AT+CSOC="+socketId+",1,1,1\r\n");
+  //创建tcp，id=0
+  DTU.sendMsg("AT+CSOC=1,1,1\r\n");
   readstr = "";
   readstr = DTU.waitMsg(1000);
   Serial.print(readstr);
 
-  //连接平台，id=1测试平台'netlab.luatos.com'
+  //连接平台，id=0测试平台'netlab.luatos.com'
   DTU.sendMsg("AT+CSOCON="+socketId+","+port+","+url+"\r\n");
   readstr = "";
   readstr = DTU.waitMsg(1000);
   Serial.print(readstr);
 
-  //发送数据，id=1,要求hex码
+  //发送数据，id=0,要求hex码
   DTU.sendMsg("AT+CSOSEND="+socketId+",6,7E117E\r\n");
   readstr = "";
   readstr = DTU.waitMsg(5000);
@@ -69,7 +69,7 @@ void loop()
   delay(5000);
   Serial.println("end delay 5s");
 
-  //销毁连接，id=1
+  //销毁连接，id=0
   DTU.sendMsg("AT+CSOCL="+socketId+",\r\n");
   readstr = "";
   readstr = DTU.waitMsg(1000);
